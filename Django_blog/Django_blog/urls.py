f5f86411a9603d398af574abf87fa2fc4bd74ab1
mahmoudadev <mahmoud.abdelmoneim.dev@gmail.com>
2018-02-14 16:29:14 +0200

    updating admin area

--- Django_blog/Django_blog/urls.py
+++ Django_blog/Django_blog/urls.py
@@ -17,5 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    ##url(r'^/', include('Blog/', 'jet')),
+    url(r'^jet/', include('jet.urls', 'jet')),
+    url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     url(r'^admin/', include(admin.site.urls)),
 ]
