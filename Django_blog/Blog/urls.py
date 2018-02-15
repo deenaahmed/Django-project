8699132e1264f55138dfaa2e1d57860b142bad05
deenaahmed <deenaahmedkamal8@gmail.com>
2018-02-15 19:54:59 +0200

    ajax not fixed

--- Django_blog/Blog/urls.py
+++ Django_blog/Blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^postpage/1$',views.postPage),
     url(r'postpage/(?P<post_id>[0-9]+)$',views.postPage),
+    url(r'postpage/(?P<post_id>[0-9]+)/(?P<user_id>[0-9]+)$',views.new_comment),
+
 ]
 
--- Django_blog/Blog/views.py
+++ Django_blog/Blog/views.py
@@ -3,30 +3,48 @@ from django.http import HttpResponse
 from .models import *
 from .forms import commentform
 from django.http import HttpResponseRedirect
+
 # Create your views here.
 
 
 def postPage(request,post_id):
 	ob = Post.objects.get(id=post_id)
 	ob1 = Comment.objects.raw("select * from Blog_comment where post_id=post_id")
-	ob2 = Reply.objects.all()  #7tb2a ashal lw 3ndna post id raw("select * from Blog_reply where post_id=post_id")
+	ob2 = Reply.objects.all()  
 	context = {'post_list':ob,
 	'comment_list':ob1,
-	'reply_list':ob2,}
+	'reply_list':ob2,
+	}
 	
 	return render(request, 'postpage.html', context)
 
-#def new_comment(request,post_id,user_id):
-#if request.method=="POST":
-#		body = request.POST['commentbody']	
-#		post = post_id
-#		user = user_id
-#		Comment.objects.create{
-#		body=body,
-#		post=post,
-#		user=user,
-#		}	
-#		return HttpResponse('')
+def new_comment(request,post_id,user_id):
+	ob = Post.objects.get(id=post_id)
+	ob1 = Comment.objects.raw("select * from Blog_comment where post_id=post_id")
+	ob2 = Reply.objects.all()  
+	context1 = {'post_list':ob,
+	'comment_list':ob1,
+	'reply_list':ob2,
+	}
+	if request.method=="POST":
+		
+		body = request.POST['commentbody']	
+		post = post_id
+		user = user_id
+		Comment.objects.create(body=body,
+		post=post,
+		user=user)
+	#	context = {'body':body,
+			#'post':post,
+			#'user':user,
+			#}
+			#body=body,
+			#post=post,
+			#user=user,
+			#}	
+		return HttpResponse("hello")
+	#return render(request, 'postpage.html',context1)
+
 	
 
 #def comment_delete(request,comment_id):
