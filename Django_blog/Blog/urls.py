01440a9345dec1b8643cc660abef1f0141a1f8fd
ayaragab <aya.r.gad.94@gmail.com>
2018-02-18 15:23:16 +0200

    committing my own changes

--- Django_blog/Blog/urls.py
+++ Django_blog/Blog/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^home/$', views.allPosts),
     url(r'^search/$', views.search),
+    url(r'^likepost/$', views.likePost, name='likepost'),   # likepost view at /likepost
+    url(r'^dislikePost/$', views.dislikePost, name='dislikepost'),   # likepost view at /likepost
 
 
 ]
--- Django_blog/Blog/views.py
+++ Django_blog/Blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
 
-from .models import Post
+from .models import Post,Like
 import re
 
 from django.db.models import Q
@@ -21,6 +21,34 @@ def search(request):
     context = {"allPosts": found_entries}
     return render(request, "blog/home.html", context)
 
+
+def likePost(request):
+        if request.method == 'GET':
+               post_id = request.GET['post_id']
+               likedpost = Post.objects.get(pk=post_id) #getting the liked posts
+               likeObj = Like(post=likedpost) # Creating Like Object
+               likeobj.state=1
+               likeObj.save()  # saving it to store in database
+               
+               return HttpResponse("Success!") # Sending an success response
+        else:
+               return HttpResponse("Request method is not a GET")
+
+def dislikePost(request):
+    counter=0
+    if request.method == 'GET':
+        post_id = request.GET['post_id']
+        dislikedpost = Post.objects.get(pk=post_id) #getting the disliked posts
+        dislikeObj = Like(post=dislikedpost) # Creating DisLike Object
+        dislikeobj.state=0
+        counter+=1
+        if (counter>10):
+            dislikedpost.delete()
+        else:
+             dislikeObj.save()  # saving it to store in database
+             return HttpResponse("Success!") # Sending an success response
+    else:
+        return HttpResponse("Request method is not a GET")
 
 
 
